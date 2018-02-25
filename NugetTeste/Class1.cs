43f84898bd5e020369f225c198caeeef19c9c1bc
Wemerson Michael Santos <santoswemerson30@gmail.com>
2018-02-25 00:29:27 -0300

    primeiro commit

---
+++ NugetTeste/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NugetTeste
+{
+    public class Teste
+    {
+        private string _texto;
+
+        public Teste(string texto)
+        {
+            _texto = texto;
+        }
+
+        public override string ToString()
+        {
+            return _texto;
+        }
+    }
+}
